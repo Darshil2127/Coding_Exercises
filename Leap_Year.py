d76d5011540e40cf13a0672814a0c6c35aaa36c7
Darshil2127 <darshilparikh2112@gmail.com>
2022-11-18 00:39:32 +1300

    Revert "Day 2"
    This reverts commit 0e37ef9d83604dab1a933a4ae66da070ea949455.

--- Leap_Year.py
+++
@@ -1,8 +0,0 @@
-year = int(input("Please enter the year = "))
-
-if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
-    print("Its a leap year")
-else:
-    print("Its not a leap year")
-
-
--- Odd_Even.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Enter the number = "))
-
-if number % 2 == 1:
-    print("The number is Odd number")
-else:
-    print("The number is Even number")
