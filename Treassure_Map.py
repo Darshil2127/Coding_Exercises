3e9c11f0bd47920ad354eeb632a6d50b3b25458a
Darshil2127 <darshilparikh2112@gmail.com>
2022-11-20 23:54:14 +1300

    Day 3

---
+++ Treassure_Map.py
@@ -0,0 +1,18 @@
+
+
+row1 = ["⬜","⬜", "⬜"]
+row2 = ["⬜","⬜", "⬜"]
+row3 = ["⬜","⬜", "⬜"]
+
+map = [row1,row2,row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+positon = input("Where you want to hide your tressure?")
+
+horizontal = int(positon[0])
+vertical = int(positon[1])
+
+map[vertical - 1][horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
