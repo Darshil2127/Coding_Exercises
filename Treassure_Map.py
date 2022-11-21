32de4a79d08b2d6f04315e117040c5daa11e396b
Darshil2127 <darshilparikh2112@gmail.com>
2022-11-21 22:55:47 +1300

    Day 4

--- Treassure_Map.py
+++ Treassure_Map.py
@@ -12,7 +12,10 @@ positon = input("Where you want to hide your tressure?")
 
 horizontal = int(positon[0])
 vertical = int(positon[1])
-
-map[vertical - 1][horizontal - 1] = "X"
+# you will input column or row 31 or 13, but there is 0,1,2 in column and row that is why we need to give this
+# File "C:\Users\StarInfinity\PycharmProjects\Coding_Exercises\Treassure_Map.py", line 16, in <module>
+#     map[vertical][horizontal] = "X"
+# IndexError: list assignment index out of range
+map[vertical-1][horizontal-1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
